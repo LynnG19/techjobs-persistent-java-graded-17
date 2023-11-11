fd20e3fcc69ed4e5a5da79a191dfbae151b472c7
LynnG19 <antiphreeeze@gmail.com>
2023-11-11 00:04:33 -0600

    2

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,29 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank(message = "Location is required")
+    @Size(max = 100, message = "Location exceeds character max")
+    private String location;
+
+
+    public Employer() {
+
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+
+
 }
