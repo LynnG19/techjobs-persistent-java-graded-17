6531c29a0a4d59d070c69f035880058af8491cf7
LynnG19 <antiphreeeze@gmail.com>
2023-11-11 19:25:33 -0600

    6

--- queries.sql
+++ queries.sql
@@ -1,16 +1,17 @@
 --Part 1
 
-id INT PRIMARY KEY,
+id INT,
 name VARCHAR(255),
 employer VARCHAR(255),
-location VARCHAR(255),
-position_type VARCHAR(255),
 skill VARCHAR(255)
 
 
 
 
 --Part 2
+
+
+
 
 --Part 3
 
