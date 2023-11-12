df8829db76e7d5e8dd562b6c83b25054c0a6f245
LynnG19 <antiphreeeze@gmail.com>
2023-11-11 19:56:23 -0600

    7

--- queries.sql
+++ queries.sql
@@ -3,13 +3,15 @@
 id INT,
 name VARCHAR(255),
 employer VARCHAR(255),
-skill VARCHAR(255)
+skills VARCHAR(255)
 
 
 
 
 --Part 2
-
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 
 
 
