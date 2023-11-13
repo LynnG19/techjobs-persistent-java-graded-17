caf1129ff53dd193ad5e3029e40ee016b3522a41
LynnG19 <antiphreeeze@gmail.com>
2023-11-12 21:08:40 -0600

    a

--- queries.sql
+++ queries.sql
@@ -15,7 +15,7 @@ WHERE location = "St. Louis City";
 DROP table job;
 
 --Part 4
-SELECT skill
+SELECT skill.name
 FROM skill
 INNER JOIN job_skills ON skill.id = job_skills.skills_id
 WHERE job_skills.job_id IS NOT NULL
